78911d3004568f057ccaba6f2fab8708887b533c
wukefei <3457447530@qq.com>
2020-05-13 22:55:32 +0800

    information

--- lab7/lab07.js
+++ lab7/lab07.js
@@ -10,59 +10,61 @@ const flexContainer = document.getElementsByClassName("flex-container")[0];
 function test() {
     let l_works = works.length;
     for (let i = 0; i < l_works; i++) {
-        // let h4Element = document.createElement("h4");
-        // let h4Inner = "Genre : " + works[i].tips;
-        // let h4Node = document.createTextNode(h4Inner);
-        // h4Element.appendChild(h4Node);
+        let h4Element = document.createElement("h4");
+        let h4Inner = "Genre : " + works[i].tips;
+        let h4Node = document.createTextNode(h4Inner);
+        h4Element.appendChild(h4Node);
 
-        // let innerBox1 = document.createElement("div");
-        // innerBox1.setAttribute("class", "inner-box");
-        // let h3Element1 = document.createElement("h3");
-        // let h3Node1 = document.createTextNode(works[i].author + " ");
-        // h3Element1.appendChild(h3Node1);
-        // let h5Element = document.createElement("h5");
-        // let h5Node = document.createTextNode(" lifetime " + works[i].lifetime);
-        // h5Element.appendChild(h5Node);
-        // h3Element1.appendChild(h5Element);
-        // innerBox1.appendChild(h3Element1);
+        let innerBox1 = document.createElement("div");
+        innerBox1.setAttribute("class", "inner-box");
+        let h3Element1 = document.createElement("h3");
+        let h3Node1 = document.createTextNode(works[i].author + " ");
+        h3Element1.style.display = 'inline-block';
+        h3Element1.appendChild(h3Node1);
+        let h5Element = document.createElement("h5");
+        let h5Node = document.createTextNode(" lifetime " + works[i].lifetime);
+        h5Element.style.display = 'inline-block';
+        h5Element.appendChild(h5Node);
+        h3Element1.appendChild(h5Element);
+        innerBox1.appendChild(h3Element1);
 
-        // let innerBox2 = document.createElement("div");
-        // innerBox2.setAttribute("class", "inner-box");
-        // let h3Element2 = document.createElement("h3");
-        // let h3Node2 = document.createTextNode("Popular photos");
-        // h3Element2.appendChild(h3Node2);
-        // innerBox2.appendChild(h3Element2);
-
-        // let photos = works[i].photos;
-        // let l_photos = photos.length;
-        // for (let j = 0; j < l_photos; j++) {
-        //     let imgElement = document.createElement("img");
-        //     imgElement.setAttribute("class", "photo");
-        //     imgElement.setAttribute("src", photos[j]);
-        //     innerBox2.appendChild(imgElement);
-        // }
-
-        // let buttonElement = document.createElement("button");
-        // let buttonNode = document.createTextNode("visit");
-        // buttonElement.appendChild(buttonNode);
-
-        // let itemElement = document.createElement("div");
-        // itemElement.setAttribute("class", "item");
-        // itemElement.appendChild(h4Element);
-        // itemElement.appendChild(innerBox1);
-        // itemElement.appendChild(innerBox2);
-        // itemElement.appendChild(buttonElement);
-
-        // flexContainer.appendChild(itemElement);
-
-        document.write("<div class='item'><h4>Genre : " + works[i].tips + "</h4><div class='inner-box'><h3 style='display: inline-block'>" + works[i].author + "　<h5 style='display: inline-block'>  lifetime:" + works[i].lifetime + "</h5></h3></div><div class='inner-box'><h3>Popular photos</h3>")
+        let innerBox2 = document.createElement("div");
+        innerBox2.setAttribute("class", "inner-box");
+        let h3Element2 = document.createElement("h3");
+        let h3Node2 = document.createTextNode("Popular photos");
+        h3Element2.appendChild(h3Node2);
+        innerBox2.appendChild(h3Element2);
 
         let photos = works[i].photos;
         let l_photos = photos.length;
         for (let j = 0; j < l_photos; j++) {
-            document.write("<img class='photo' src=" + photos[j] + "></img>");
+            let imgElement = document.createElement("img");
+            imgElement.setAttribute("class", "photo");
+            imgElement.setAttribute("src", photos[j]);
+            innerBox2.appendChild(imgElement);
         }
-        document.write("</div><button>Visit</button></div>");
+
+        let buttonElement = document.createElement("button");
+        let buttonNode = document.createTextNode("visit");
+        buttonElement.appendChild(buttonNode);
+
+        let itemElement = document.createElement("div");
+        itemElement.setAttribute("class", "item");
+        itemElement.appendChild(h4Element);
+        itemElement.appendChild(innerBox1);
+        itemElement.appendChild(innerBox2);
+        itemElement.appendChild(buttonElement);
+
+        flexContainer.appendChild(itemElement);
+
+        // document.write("<div class='item'><h4>Genre : " + works[i].tips + "</h4><div class='inner-box'><h3 style='display: inline-block'>" + works[i].author + "　<h5 style='display: inline-block'>  lifetime:" + works[i].lifetime + "</h5></h3></div><div class='inner-box'><h3>Popular photos</h3>")
+
+        // let photos = works[i].photos;
+        // let l_photos = photos.length;
+        // for (let j = 0; j < l_photos; j++) {
+        //     document.write("<img class='photo' src=" + photos[j] + "></img>");
+        // }
+        // document.write("</div><button>Visit</button></div>");
     }
 }
 
