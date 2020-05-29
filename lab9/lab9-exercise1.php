8b40eb3c429e7cc228b135a98a651ca1bf1840d7
wukefei <3457447530@qq.com>
2020-05-29 22:25:52 +0800

    lab9

---
+++ lab9/lab9-exercise1.php
@@ -0,0 +1,20 @@
+<html>
+
+<head>
+    <title>Exercise 1</title>
+</head>
+
+<body>
+    <h1>Regular HTML section (outside &lt;?php ... ?&gt; tags)</h1>
+    <p>You can type regular HTML here and it will show up</p>
+
+    <h1>PHP section (inside &lt;?php ... ?&gt; tags)</h1>
+    <?php
+    //this is a php comment IN tags (will not appear)
+    $remaining = date("L") ? 366 - date("z") : 365 - date("z");
+    echo "There are " . $remaining . " days left in the year";
+    echo "<br>"; //notice we must echo tags in php.
+    ?>
+</body>
+
+</html>
